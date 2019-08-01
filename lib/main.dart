85b0ff902bf04ba00b9c81ebb35585e7acf3d06f
AMOREM <amorem@amorem.com>
2019-08-01 20:24:21 +0200

    String Interpolation

--- lib/main.dart
+++ lib/main.dart
@@ -41,9 +41,43 @@ class MyHomePage extends StatelessWidget {
               child: Container(width: 100, child: Text('Chart !')),
             ),
           ),
-          Card(
-            child: Text('List of transactions !!!'),
-            color: Colors.red,
+          Column(
+            children: transactions.map((tx) {
+              return Card(
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      margin:
+                          EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Colors.purple, width: 2)),
+                      padding: EdgeInsets.all(10),
+                      child: Text(
+                        "\$${tx.amount}",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Colors.purple),
+                      ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          tx.title,
+                          style: TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          tx.date.toString(),
+                          style: TextStyle(color: Colors.grey),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              );
+            }).toList(),
           )
         ],
       ),
